956e1ab820e8b0e08902aea96a6ea4b7bf5d5a42
Mohammed Izhaan Kizhakke Kottayil <mohammedIzhaan2018@outlook.com>
2025-02-16 14:49:44 +0400

    Message Correction

--- CurrencyExchange.BusinessModels/Exception/AppException.cs
+++ CurrencyExchange.BusinessModels/Exception/AppException.cs
@@ -8,10 +8,11 @@ namespace CurrencyExchange.BusinessModels
     public class ApiException : Exception
     {
         public HttpStatusCode StatusCode { get; }
-
-        public ApiException(HttpStatusCode statusCode, string message) 
+        
+        public ApiException(HttpStatusCode statusCode, string message) :base(message)
         {
             StatusCode = statusCode;
+           
         }
     }
 }
